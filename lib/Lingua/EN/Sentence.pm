e82b200eb9f26d4bef0bf8b871ba5092cc7a0ff8
Shlomo Yona <shlomoy@cpan.org>
2001-11-18 08:06:38 -0800

    import Lingua-EN-Sentence 0.17 from CPAN
    git-cpan-module:   Lingua-EN-Sentence
    git-cpan-version:  0.17
    git-cpan-authorid: SHLOMOY
    git-cpan-file:     authors/id/S/SH/SHLOMOY/Lingua-EN-Sentence-0.17.tar.gz

--- lib/Lingua/EN/Sentence.pm
+++ lib/Lingua/EN/Sentence.pm
@@ -133,7 +133,7 @@ require Exporter;
 use vars qw/$VERSION @ISA @EXPORT_OK $EOS $AP $P $PAP @ABBREVIATIONS/;
 use Carp qw/cluck/;
 
-$VERSION = '0.16';
+$VERSION = '0.17';
 @ISA = qw( Exporter );
 @EXPORT_OK = qw( get_sentences 
 		add_acronyms get_acronyms set_acronyms
@@ -241,8 +241,8 @@ sub remove_false_end_of_sentence {
 ##	$marked_segment=~s/(\s\w$PAP)$EOS/$1/sg; 
 ##	## don't do u.s.a.
 ##	$marked_segment=~s/(\.\w$PAP)$EOS/$1/sg; 
-	$marked_segment=~s/(\W\w$PAP)$EOS/$1/sg;
-	$marked_segment=~s/(\W\w$P)$EOS/$1/sg;         
+	$marked_segment=~s/([^\wיכאש]\w$PAP)$EOS/$1/sg;
+	$marked_segment=~s/([^\wיכאש]\w$P)$EOS/$1/sg;         
 
 	# don't split |John P. Stenbit| into |John P.| and |Stenbit|
 	$marked_segment=~s/([A-Z]\w+\s+\S$P\s*)$EOS(\s*[A-Z])/$1$2/sg; 
