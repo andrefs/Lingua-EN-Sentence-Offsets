ab41c937a7101b47f4c946ebd0e3196ad6b17215
André Santos <andrefs@cpan.org>
2012-04-19 23:24:57 +0100

    finished modifying original functions from L::EN::S to work with offsets small test verifies if get_sentences output remains the same (TODO: improve test text)

--- lib/Lingua/EN/Sentence/Offsets.pm
+++ lib/Lingua/EN/Sentence/Offsets.pm
@@ -3,30 +3,22 @@ package Lingua::EN::Sentence::Offsets;
 require Exporter;
 
 #ABSTRACT: Finds sentence boundaris, and returns their offsets.
-our ($VERSION,@ISA,$EOS,$LOC,$AP,$P,$PAP,@ABBREVIATIONS);
+my ($EOS,$AP,$P,$PAP,@ABBREVIATIONS);
 use Carp qw/cluck/;
 use feature qw/say/;
-
+use utf8::all;
 
 use base 'Exporter';
 #@EXPORT_OK = qw/
 our @EXPORT = qw/
 				get_sentences 
+				get_offsets
 				add_acronyms 
 				get_acronyms 
 				set_acronyms
-				get_EOS 
-				set_EOS
-				offsets2sentences
-				initial_offsets 
-				_get_text
-				adjust_offsets
 			/;
 
-$EOS="\001";
-$P = q/[\.!?]/;			## PUNCTUATION
-$AP = q/(?:'|"|»|\)|\]|\})?/;	## AFTER PUNCTUATION
-$PAP = $P.$AP;
+$EOS="\001";$P = q/[\.!?]/;$AP = q/(?:'|"|»|\)|\]|\})?/;$PAP = $P.$AP;
 
 my @PEOPLE = ( 'jr', 'mr', 'mrs', 'ms', 'dr', 'prof', 'sr', "sens?", "reps?", 'gov',
 		"attys?", 'supt',  'det', 'rev' );
@@ -50,12 +42,23 @@ my @MISC = ( 'vs', 'etc', 'no', 'esp' );
 
 @ABBREVIATIONS = (@PEOPLE, @ARMY, @INSTITUTES, @COMPANIES, @PLACES, @MONTHS, @MISC ); 
 
-# 
-# A regular expression cuts viciously the text into sentences, 
-# and then a list of rules (some of them consist of a list of abbreviations)
-# is applied on the marked text in order to fix end-of-sentence markings on 
-# places which are not indeed end-of-sentence.
-#------------------------------------------------------------------------------
+=head2 get_offsets
+
+Takes text input and returns reference to array containin pairs of character
+offsets, corresponding to the sentences start and end positions.
+
+=cut
+
+sub get_offsets {
+	my ($text) = @_;
+	return [] unless defined $text;
+	my $offsets = initial_offsets($text);
+	remove_false_eos($text,$offsets);
+	split_unsplit_stuff($text,$offsets);
+	adjust_offsets($text,$offsets);
+	return $offsets;
+}
+
 
 =head2 get_sentences 
 
@@ -64,14 +67,10 @@ Takes text input and splits it into sentences.
 =cut
 
 sub get_sentences {
-	my ($text)=@_;
-	return [] unless defined $text;
-	my $marked_text = first_sentence_breaking($text);
-	my $fixed_marked_text = remove_false_end_of_sentence($marked_text);
-	$fixed_marked_text = split_unsplit_stuff($fixed_marked_text);
-	my @sentences = split(/$EOS/,$fixed_marked_text);
-	my $cleaned_sentences = clean_sentences(\@sentences);
-	return $cleaned_sentences;
+	my ($text) = @_;
+	my $offsets = get_offsets($text);
+	my $sentences = offsets2sentences($text,$offsets);
+	return $sentences;
 }
 
 =head2 add_acronyms 
@@ -105,106 +104,140 @@ sub set_acronyms {
 	@ABBREVIATIONS=@_;
 }
 
-#==============================================================================
-#
-# Private methods
-#
-#==============================================================================
+=head2 remove_false_eos
 
-## Please email me any suggestions for optimizing these RegExps.
-sub remove_false_end_of_sentence {
-	my ($marked_segment) = @_;
-##	## don't do u.s.a.
-##	$marked_segment=~s/(\.\w$PAP)$EOS/$1/sg; 
-	$marked_segment=~s/([^-\w]\w$PAP\s)$EOS/$1/sg;
-	$marked_segment=~s/([^-\w]\w$P)$EOS/$1/sg;         
+=cut
 
-	# don't plit after a white-space followed by a single letter followed
-	# by a dot followed by another whitespace.
-	$marked_segment=~s/(\s\w\.\s+)$EOS/$1/sg; 
+sub remove_false_eos {
+	my ($text,$offsets) = @_;
+	my $size = @$offsets;
+	for(my $i=0; $i<$size-1; $i++){
+		my $start  = $offsets->[$i][0];
+		my $end    = $offsets->[$i][1];
+		my $length = $end-$start;
+		my $s = substr($text,$start,$length);
+		my $j=$i+1;
 
-	# fix: bla bla... yada yada
-	$marked_segment=~s/(\.\.\. )$EOS([[:lower:]])/$1$2/sg; 
-	# fix "." "?" "!"
-	$marked_segment=~s/(['"]$P['"]\s+)$EOS/$1/sg;
-	## fix where abbreviations exist
-	foreach (@ABBREVIATIONS) { $marked_segment=~s/(\b$_$PAP\s)$EOS/$1/isg; }
-	
-	# don't break after quote unless its a capital letter.
-	$marked_segment=~s/(["']\s*)$EOS(\s*[[:lower:]])/$1$2/sg;
+		my $unsplit = 0;
+		$unsplit = 1 if $s =~ /[^-\w]\w$PAP\s$/s;
+		$unsplit = 1 if $s =~ /[^-\w]\w$P$/s;
 
-	# don't break: text . . some more text.
-	$marked_segment=~s/(\s\.\s)$EOS(\s*)/$1$2/sg;
+		# don't split after a white-space followed by a single letter followed
+		# by a dot followed by another whitespace.
+		$unsplit = 1 if $s =~ /\s\w\.\s+$/;
 
-	$marked_segment=~s/(\s$PAP\s)$EOS/$1/sg;
-	return $marked_segment;
+		# fix: bla bla... yada yada
+		my $t = substr($text,$offsets->[$j][0], $offsets->[$j][1]-$offsets->[$j][0]);
+		$unsplit = 1 if $s =~ /\.\.\.\s*$/s and $t =~ /^\s*[[:lower:]]/s;
+
+		# fix "." "?" "!"
+		$unsplit = 1 if $s =~ m{['"]$P['"]\s+$}s;
+
+		## fix where abbreviations exist
+		foreach (@ABBREVIATIONS){ $unsplit = $1 if $s =~ /\b$_$PAP\s$/is; }
+
+		# don't break after quote unless its a capital letter.
+		$unsplit = 1 if $s =~ /["']\s*$/s and $t =~ /^\s*[[:lower:]]/s;
+
+		# don't break: text . . some more text.
+		$unsplit = 1 if $s =~ /\s\.\s$/s and $t =~ /^\s*/s;
+
+		$unsplit = 1 if $s =~ /\s$PAP\s$/s;
+
+		_merge_forward($offsets,$i) if $unsplit;
+	}
+	for(my $i=0; $i<$size; $i++){ splice @$offsets, $i,1 unless defined($offsets->[$i]); }
 }
+
+sub _merge_forward {
+	my ($offsets,$i) = @_;
+	my $j = $i+1;
+	return $offsets unless defined($offsets->[$j]);
+
+	$offsets->[$j][0] = $offsets->[$i][0];
+	delete $offsets->[$i];
+
+	#splice @$offsets, $i, 1;
+}
+
+=head2 split_unsplit_stuff
+
+Finds additional split points in the middle of previously defined sentences.
+
+=cut
 
 sub split_unsplit_stuff {
-	my ($text) = @_;
+	my ($text,$offsets) = @_;
+	my $size = @$offsets;
+	for(my $i=0; $i<$size-1; $i++){
+		my $start  = $offsets->[$i][0];
+		my $end    = $offsets->[$i][1];
+		my $length = $end-$start;
+		my $s = substr($text,$start,$length);
 
-	$text=~s/(\D\d+)($P)(\s+)/$1$2$EOS$3/sg;
-	$text=~s/($PAP\s)(\s*\()/$1$EOS$2/gs;
-	$text=~s/('\w$P)(\s)/$1$EOS$2/gs;
+		my $split_points = [];
+		while($s =~ /(\D\d+$P)(\s+)/g){
+			$end   = $+[1];
+			$start = $-[2];
+			push @$split_points,[$end,$start];
+		}
+		while($s =~ /($PAP\s)(\s*\()/g){
+			$end   = $+[1];
+			$start = $-[2];
+			push @$split_points,[$end,$start];
+		}
+		while($s =~ /('\w$P)(\s)/g){
+			$end   = $+[1];
+			$start = $-[2];
+			push @$split_points,[$end,$start];
+		}
+		while($s =~ /(\sno\.)(\s+)(?!\d)/g){
+			$end   = $+[1];
+			$start = $-[2];
+			push @$split_points,[$end,$start];
+		}
 
-
-	$text=~s/(\sno\.)(\s+)(?!\d)/$1$EOS$2/gis;
-
-##	# split where single capital letter followed by dot makes sense to break.
-##	# notice these are exceptions to the general rule NOT to split on single
-##	# letter.
-##	# notice also that sibgle letter M is missing here, due to French 'mister'
-##	# which is representes as M.
-##	#
-##	# the rule will not split on names begining or containing 
-##	# single capital letter dot in the first or second name
-##	# assuming 2 or three word name.
-##	$text=~s/(\s[[:lower:]]\w+\s+[^[[:^upper:]M]\.)(?!\s+[[:upper:]]\.)/$1$EOS/sg;
-
-
-	# add EOS when you see "a.m." or "p.m." followed by a capital letter.
-	$text=~s/([ap]\.m\.\s+)([[:upper:]])/$1$EOS$2/gs;
-
-	return $text;
+		foreach( sort { $a->[0] <=> $b->[0] } @$split_points){
+			_split_sentence($offsets,$i,$_->[0],$_->[1]);
+		}
+	}
 }
 
-# sub clean_sentences {
-# 	my ($sentences) = @_;
-# 		my $cleaned_sentences;
-# 		foreach my $s (@$sentences) {
-# 			next if not defined $s;
-# 			next if $s!~m/\w+/;
-# 			$s=~s/^\s*//;
-# 			$s=~s/\s*$//;
-# ##			$s=~s/\s+/ /g;
-# 			push @$cleaned_sentences,$s;
-# 		}
-# 	return $cleaned_sentences;
-# }
+
+
+sub _split_sentence {
+	my ($offsets,$i,$end1,$start2) = @_;
+	my $end2 = $offsets->[$i][1];
+	$offsets->[$i][1] = $end1;
+	$start2 //= $end1;
+	push $offsets, [$start2, $end2];
+}
+
+=head2 adjust_offsets 
+
+Minor adjusts to offsets (leading/trailing whitespace, etc)
+
+=cut
 
 sub adjust_offsets {
 	my ($text,$offsets) = @_;
 	my $new_offsets = [];
-	foreach (@$offsets){
-		my $start  = $_->[0];
-		my $end    = $_->[1];
+	my $size = @$offsets;
+	for(my $i=0; $i<$size; $i++){
+		my $start  = $offsets->[$i][0];
+		my $end    = $offsets->[$i][1];
 		my $length = $end - $start;
 		my $s = substr($text,$start,$length);
-		next if $s !~ /\w+/;
+		if ($s !~ /\w+/){
+			delete $offsets->[$i];
+			next;
+		}
 		$s =~ /^(\s*).*?(\s*)$/;
 		if(defined($1)){ $start += length($1); }
 		if(defined($2)){ $end   -= length($2); }
-		push @$new_offsets, [$start, $end];
+		$offsets->[$i] = [$start, $end];
 	}
-	return $new_offsets;
-}
-
-sub first_sentence_breaking {
-	my ($text) = @_;
-	$text=~s/\n\s*\n/$EOS/gs;	## double new-line means a different sentence.
-	$text=~s/($PAP\s)/$1$EOS/gs;
-	$text=~s/(\s\w$P)/$1$EOS/gs; # breake also when single letter comes before punc.
-	return $text;
+	for(my $i=0; $i<$size; $i++){ splice @$offsets, $i,1 unless defined($offsets->[$i]); }
 }
 
 =head2 initial_offsets
@@ -221,7 +254,7 @@ sub initial_offsets {
 
 	my $start = 0;
 	#while($text =~ /(\n\s*\n)|$PAP\s()|\s\w$P()/gs){
-	while($text =~ /(\n\s*\n)|$PAP(\s+)|\s\w$P()/gs){
+	while($text =~ /(\n\s*\n)|$PAP(\s)|\s\w$P()/gs){
 
 		## double new-line means a different sentence
 		if(defined($1)){
@@ -231,7 +264,7 @@ sub initial_offsets {
 
 		## punctuation+after_punct followed by space
 		elsif(defined($2)){
-			push @$offsets, [$start, $-[2]];
+			push @$offsets, [$start, $+[2]];
 			$start = $+[2];
 		}
 
@@ -264,18 +297,5 @@ sub offsets2sentences {
 	}
 	return $sentences;
 }
-
-sub _get_text {
-	open my $fh, '<', 't/text';
-	my $text = join '', <$fh>;
-	close $fh;
-	return $text;
-}
-
-#==============================================================================
-#
-# Return TRUE
-#
-#==============================================================================
 
 1;
